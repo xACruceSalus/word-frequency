134a893455c3fae4d592e407276a44b4ea1356fc
Zack Cooper <zackjcooper@gmail.com>
2015-01-15 13:16:17 -0500

    cleaned up variable names

--- word_frequency.py
+++ word_frequency.py
@@ -3,20 +3,19 @@ def read_file(a_file):
     produce a count of the top twenty most used words,
     and plot them on a histogram."""
 
-    output = []
+    word_list = []
 
     with open(a_file) as file:
         all_lines = file.readlines()
 
-        for a_string in all_lines:
-            a_string = a_string.lower().split()
-            output.extend(a_string)
+        for a_row in all_lines:
+            a_string = a_row.split()
+            word_list.extend(a_string)
 
-        output = word_frequency(output)
-        print(output)
-        output = top_twenty_pretty(output)
+        word_count = word_frequency(word_list)
+        top_twenty = top_twenty_pretty(word_count)
 
-    return output
+    return top_twenty
 
 
 def word_frequency(a_string):
@@ -24,29 +23,24 @@ def word_frequency(a_string):
     of the top 20 most used words."""
 
     new_dict = {}
-    clean_list = []
-
-    for word in a_string:
-        word = word_cleanup(word)
-        clean_list.append(word)
+    clean_list = [word_cleanup(word.lower()) for word in a_string]
 
     for word in clean_list:
-        if word == '':
-            continue
         if word in new_dict:
             new_dict[word] += 1
         else:
             new_dict[word] = 1
 
-    new_dict = sorted(new_dict.items(), key= lambda x: x[1], reverse = True)
+    new_dict = sorted(new_dict.items(), key=lambda x: x[1], reverse=True)
 
     return new_dict[:20]
+
 
 def word_cleanup(a_string):
     """Cleans up a string, by removing the specified characters"""
 
     bad_chars = [".", ",", "?", "!", "@", "&", "%",
-                "$", "' ", " - ", " '"]
+                 "$", "' ", " - ", " '"]
 
     for char in bad_chars:
         if char in ["' ", " - ", " '"]:
@@ -54,6 +48,7 @@ def word_cleanup(a_string):
         else:
             a_string = a_string.replace(char, '')
     return a_string
+
 
 def top_twenty_pretty(a_list):
     """Takes in a list of tuples and returns
@@ -67,7 +62,7 @@ def top_twenty_pretty(a_list):
     sum_list = sum(values_list)
 
     if max(values_list) > 50:
-        scale = 50 / max(values_list) 
+        scale = 50 / max(values_list)
         for a, b in a_list:
             print(a, int(b * scale) * '#')
     else:
@@ -75,5 +70,6 @@ def top_twenty_pretty(a_list):
             print(i[0], i[1] * '#')
 
     return sum_list
+
 
 read_file("sample.txt")
