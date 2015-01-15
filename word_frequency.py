69d6cf11635160b5516626bff6fd32e42f5f9d09
Zack Cooper <zackjcooper@gmail.com>
2015-01-15 17:59:29 -0500

    updated to end in dict

--- word_frequency.py
+++ word_frequency.py
@@ -1,43 +1,42 @@
+from collections import Counter
+
+
 def read_file(a_file):
-    """This function's job is to read the file, clean the file,
+    """This function's job is to read a file, clean the file,
     produce a count of the top twenty most used words,
     and plot them on a histogram."""
 
-    word_list = []
-
     with open(a_file) as file:
-        all_lines = file.readlines()
+        all_lines = file.read()
+        word_count = word_frequency(all_lines)
 
-        for a_row in all_lines:
-            a_string = a_row.split()
-            word_list.extend(a_string)
-
-        word_count = word_frequency(word_list)
-        top_twenty = top_twenty_pretty(word_count)
-
-    return top_twenty
+    return word_count
 
 
 def word_frequency(a_string):
-    """Ingests a list and returns a count
+    """Ingests a string and returns a count
     of the top 20 most used words."""
 
-    new_dict = {}
-    clean_list = [word_cleanup(word.lower()) for word in a_string]
+    return_amount = 20
+    dict_w_word_count = {}
 
-    for word in clean_list:
-        if word in new_dict:
-            new_dict[word] += 1
-        else:
-            new_dict[word] = 1
+    split_string = a_string.split()
+    clean_list = [word_cleanup(word) for word in split_string]
 
-    new_dict = sorted(new_dict.items(), key=lambda x: x[1], reverse=True)
+    list_w_word_count = Counter(clean_list)
+    list_w_word_count = sorted(list_w_word_count.items(),
+                               key=lambda x: x[1], reverse=True)
 
-    return new_dict[:20]
+    top_twenty_pretty(list_w_word_count[:return_amount])
+
+    for name,num in list_w_word_count[:return_amount]:
+        dict_w_word_count.update({name: num})
+
+    return dict_w_word_count
 
 
 def word_cleanup(a_string):
-    """Cleans up a string, by removing the specified characters"""
+    """Cleans up a string by removing the specified characters"""
 
     bad_chars = [".", ",", "?", "!", "@", "&", "%",
                  "$", "' ", " - ", " '"]
@@ -47,7 +46,7 @@ def word_cleanup(a_string):
             a_string = a_string.replace(char, ' ')
         else:
             a_string = a_string.replace(char, '')
-    return a_string
+    return a_string.lower()
 
 
 def top_twenty_pretty(a_list):
@@ -72,4 +71,4 @@ def top_twenty_pretty(a_list):
     return sum_list
 
 
-read_file("sample.txt")
+print(read_file("sample.txt"))
